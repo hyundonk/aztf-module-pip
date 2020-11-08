92f22469039d61ed6c59d53370e8425ff0880d29
Calvin Hyundong Kim <calvin75@gmail.com>
2020-11-08 11:38:06 +0900

    Update output.tf

--- output.tf
+++ output.tf
@@ -2,11 +2,11 @@ output "public_ip" {
   value = azurerm_public_ip.public_ip
 }
 
+/*
 output "public_ip_diag" {
   value = azurerm_monitor_diagnostic_setting.diagnostics
 }
 
-/*
 output "ip_address" {
   value = azurerm_public_ip.public_ip.ip_address
 }
